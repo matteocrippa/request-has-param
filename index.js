b14dcef9bd3adc15fff3ba8e6e5a7fe3b50fa2a4
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-09-09 18:04:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ module.exports = function(params) {
 
   return function (req, res, next) {
     params.forEach(function(param) {
-      if (req.body[param] == undefined || req.body[param] == null) {
+      if (req.params[param] == undefined || req.params[param] == null) {
         next(new Error('request missing '+param));
       }
     });
     next();
   }
-
-/*function hasParam(params, req, res, next) {
-  params.forEach(function(param) {
-    if (req.params[param] == undefined || req.params[param] == null) {
-      next(new Error('request missing '+param));
-    }
-  });
-  next();
-}*/
 
 };
